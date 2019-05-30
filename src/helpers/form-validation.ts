453aa8695d4db6d4013e6f0a2a8f839ac37ef384
pixochi <kozak.jakub55@gmail.com>
2019-05-30 23:46:48 +0200

    Add a nonsense function with loops

--- src/helpers/form-validation.ts
+++ src/helpers/form-validation.ts
@@ -7,3 +7,29 @@ export const maxLength = (max: number) => (value: string) =>
 
 export const minLength = (min: number) => (value: string) =>
   value && value.length < min ? `Must be ${min} characters or more` : undefined;
+
+const FIRST_LOWER_CASE_LETTER_CHAR_CODE = 97;
+const LAST_LOWER_CASE_LETTER_CHAR_CODE = 122;
+
+export const allLowerCaseLetters = (words: string[]) => {
+  const lowerCaseWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let lowerCaseCount = 0;
+
+    for (let j = 0; j < word.length; j++) {
+      const letter = word[j];
+
+      if (letter.charCodeAt(0) >= FIRST_LOWER_CASE_LETTER_CHAR_CODE || letter.charCodeAt(0) <= LAST_LOWER_CASE_LETTER_CHAR_CODE) {
+        lowerCaseCount++;
+      }
+    }
+
+    if (lowerCaseCount === word.length) {
+      lowerCaseWords.push(word);
+    }
+  }
+
+  return lowerCaseWords;
+};
